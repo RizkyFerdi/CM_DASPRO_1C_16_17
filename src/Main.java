5070f5a2ea141a1224a3ac109fc8c01141d83315
RizkyFerdi <mazrizky46@gmail.com>
2024-12-10 13:37:05 +0700

    add tambah item

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,101 @@
 import java.util.Scanner;
 
 public class Main {
-    static String[] kolom ={"No","Nama Item","Kategori","Stok"};
-    static String[][] arrItem={{ "Kopi","Minuman" },{ "Teh","Minuman" },{ "Susu","Minuman" },{"Kacang","Makanan"}};
-    static int[] stokItem={3,4,5,6};
+    static String[] kolom = { "No", "Nama Item", "Kategori", "Stok" };
+    static String[][] arrItem = new String[10][2];
+    static {
+        arrItem[0] = new String[] { "Kopi Hitam", "Minuman" };
+        arrItem[1] = new String[] { "Cappucino", "Minuman" };
+        arrItem[2] = new String[] { "Teh Tarik", "Minuman" };
+    }
+    static int[] stokItem = new int[10];
+    static{
+        stokItem[0] = 10;
+        stokItem[1] = 5;
+        stokItem[2] = 8;
+    }
+
     static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
         pilihMenu();
 
     }
 
-    static void pilihMenu(){
+    static void pilihMenu() {
         System.out.println("===== MENU INVENTORI KAFE =====");
         System.out.println("1. Tampilkan Inventori");
         System.out.println("2. Tambah Stok untuk Item yang Ada");
         System.out.println("3. Tambah Item Baru");
-        System.out.println("5. Keluar");
+        System.out.println("4. Keluar");
+        System.out.print("Pilih Menu: ");
         int pilihMenu = sc.nextInt();
 
-        if (pilihMenu == 1){
+        if (pilihMenu == 1) {
             tampilMenu();
-        }
-        else if (pilihMenu == 2){
+        } else if (pilihMenu == 2) {
             tambahStok();
-        }
-        else if (pilihMenu == 3){
+        } else if (pilihMenu == 3) {
             for (int i = 0; i < arrItem.length; i++) {
-                
+
             }
-             tambahItemBaru();
-        }
-        else if (pilihMenu == 5){
+            tambahItemBaru();
+        } else if (pilihMenu == 5) {
             System.exit(0);
         }
 
     }
 
-    static void tampilMenu(){
-        int nomer=0;
-        int stok=0;
+    static void tampilMenu() {
+        int nomer = 0;
+        int stok = 0;
         System.out.println("No   Nama Item       Kategori        Stok");
         System.out.println("------------------------------------------");
         for (int i = 0; i < arrItem.length; i++) {
             for (int j = 0; j < arrItem[i].length; j++) {
-                if(arrItem[i][j] != null){
-
-                if (j==0) {
-                    System.out.print((++nomer)+"    ");
-                    
-                }
-                System.out.print(arrItem[i][j]);
-                for (int j2 = String.valueOf(arrItem[i][j]).length(); j2 < 16; j2++) {
-                    System.out.print(" ");
+                if (arrItem[i][j] != null) {
+                    if (j == 0) {
+                        System.out.print((++nomer) + "    ");
+                    }
+                    System.out.print(arrItem[i][j]);
+                    for (int j2 = String.valueOf(arrItem[i][j]).length(); j2 < 16; j2++) {
+                        System.out.print(" ");
+                    }
                 }
             }
-
-            }
+            if (arrItem[i][0] != null) {
+                
                 System.out.println(stokItem[stok++]);
-            
+            }
+
         }
         System.out.println();
         pilihMenu();
     }
 
-    static int tambahStok(){return 0;}
-    static void tambahItemBaru(){};
+    static int tambahStok() {
+        return 0;
+    }
+
+    static void tambahItemBaru() {
+        System.out.print("Masukkan Nama Item Baru: ");
+        String itemBaru = sc.nextLine();
+        itemBaru = sc.nextLine();
+        System.out.print("Masukkan Kategori Item Baru: ");
+        String kategori = sc.nextLine();
+        System.out.print("Masukkan Jumlah Stok Awal: ");
+        int stokBaru = sc.nextInt();
+
+        for (int i = 0; i < arrItem.length; i++) {
+            if (arrItem[i][0] == null) {
+                arrItem[i][0] = itemBaru;
+                arrItem[i][1] = kategori;
+                stokItem[i] = stokBaru;
+                break;
+            }
+        }
+        System.out.println("Item berhasil ditambahkan.");
+        pilihMenu();
+
+    };
 }
